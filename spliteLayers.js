5800ce48c2fcf88967aa1039def7a8a4f47d8d8a
Ciao <448600496@qq.com>
2020-03-12 10:05:59 +0800

    Add files via upload

---
+++ spliteLayers.js
@@ -0,0 +1,17 @@
+module.exports = [   
+    [
+		{
+			'layers': '1-1',
+		},{
+			'layers': '1-2',
+		},
+	],[
+		{
+			'layers': '2-1',
+		},{
+			'layers': '2-2',
+		},{
+			'layers': '2-3',
+		},
+	]
+]
